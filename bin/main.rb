8380d49f951f6789f33a622962968cab01ef6c4a
Stefan Dili <stefan.dili.021@gmail.com>
2020-03-19 09:06:30 +0100

    made InputOutput module, .env file with keys, .gitignore, Gemfile

---
+++ bin/main.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require 'twitter'
+require './lib/module.rb'
+require './lib/bot_class.rb'
+require 'dotenv'
+Dotenv.load
+
+bot = ImpostorBot.new
+bot.streaming
+
+
+
---
+++ lib/module.rb
@@ -0,0 +1,17 @@
+module InputOutput
+    include Twitter
+    def streaming
+        input_stream = Twitter::Streaming::Client.new(@keys)
+        input_stream.filter(track: @keyword) do |target|
+            p poster = target.user.screen_name
+            tweet = target.text
+            tweet_id = target.id
+        end
+    end
+
+    def reply
+        post = Twitter::REST::Client.new(@keys)
+        post.update("@#{poster} feeling that way is a sign that you are pushing yourself to your limits and growing. Keep it up!", in_reply_to_status_id: tweet_id)
+        p "Posted in reply to: #{tweet} by @#{poster} with an ID of: #{tweet_id}"
+    end
+end
